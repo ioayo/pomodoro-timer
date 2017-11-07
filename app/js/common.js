8552afb6de25bcf145fa8ddc0a1621b50f6b21cf
iocode <bulawake@gmail.com>
2017-11-07 16:46:12 +0800

    init

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,19 @@
-$(function() {
+var pomodoro = {
+	minutes: 0,
+	seconds: 58,
+	init: function(){
+		var self = this,
+		seconds = self.seconds,
+		minutes = self.minutes;
+		setInterval(function(){
+			console.log(minutes, seconds);
+			seconds++;
+			if (seconds > 59) {
+				minutes ++;
+				seconds = 0;
+			}	
+		}, 1000)
+	}
+};
 
-	// Custom JS
-
-});
+window.onload = pomodoro.init();
